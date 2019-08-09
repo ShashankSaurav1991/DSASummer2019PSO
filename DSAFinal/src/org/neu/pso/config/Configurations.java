92c459a0fb64b56a15746361b9f40056927a8226
AnshitNEU <srivastava.ans@northeastern.edu>
2019-08-09 18:33:04 -0400

    Model Classes and Configuration file

---
+++ DSAFinal/src/org/neu/pso/config/Configurations.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.neu.pso.config;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author anshit
+ */
+public interface Configurations {
+    int SWARM_SIZE = 50;
+	int MAX_ITERATION = 200;
+	int PROBLEM_DIMENSION = 2;
+	double C1 = 2.0;
+	double C2 = 2.0;
+	double W_UPPERBOUND = 1.0;
+	double W_LOWERBOUND = 0.0;
+        double RANGEBOUND = 150.0;
+        public double[] fitnessValueList = new double[SWARM_SIZE];
+        SwarmFunctions sFunc = new SwarmFunctions();
+
+}
---
+++ DSAFinal/src/org/neu/pso/model/PBest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.neu.pso.model;
+
+/**
+ *
+ * @author anshit
+ */
+public class PBest {
+    private int Id;
+    private double fitnessValue;
+    private Location location;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public double getFitnessValue() {
+        return fitnessValue;
+    }
+
+    public void setFitnessValue(double fitnessValue) {
+        this.fitnessValue = fitnessValue;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
