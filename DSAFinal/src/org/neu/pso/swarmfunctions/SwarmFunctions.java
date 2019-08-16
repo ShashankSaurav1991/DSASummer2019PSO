c0ad02fff5bdc518fb1fe15d0d5814a90e5cb925
Shashank Saurav <saurav.shashank15@gmail.com>
2019-08-16 18:15:53 -0400

    Updating Swarm Functions and final calculation

--- DSAFinal/src/org/neu/pso/swarmfunctions/SwarmFunctions.java
+++ DSAFinal/src/org/neu/pso/swarmfunctions/SwarmFunctions.java
@@ -7,9 +7,12 @@ package org.neu.pso.swarmfunctions;
 
 import java.util.Random;
 import java.util.Vector;
+import static org.neu.pso.config.Configurations.PROBLEM_DIMENSION;
 import static org.neu.pso.config.Configurations.SWARM_SIZE;
 import org.neu.pso.model.Location;
+import org.neu.pso.model.PBest;
 import org.neu.pso.model.Particle;
+import org.neu.pso.model.Velocity;
 
 /**
  *
@@ -17,22 +20,34 @@ import org.neu.pso.model.Particle;
  */
 public class SwarmFunctions {
     private Vector<Particle> swarm = new Vector<Particle>();
-    public static final double X_C = 100;
-    public static final double Y_C = 100; //initialize the location of living area C
-    public static final double X_D = 150;
-    public static final double Y_D = 150; //initialize the location of living area D
+    
+    public static final double S_X_LOW_LOC = 0;
+    public static final double S_X_HIGH_LOC = 200;
+    public static final double S_Y_LOW_LOC = 0;
+    public static final double S_Y_HIGH_LOC = 200; // define the range of location
+    public static final double PAR_LOW_VEL = 0;
+    public static final double PAR_HIGH_VEL = 10; // define the range of velocity
 
-    public static final double X_B = 0;
-    public static final double Y_B = 0;
-    public static final long POPULATION_B = 500;
+    public static final double PAR_ERR_TOLERANCE = 1E-20; // the smaller the tolerance, the more accurate the result, 
+    
+    
+    
+    public static final double X_NEIGHBOURHOOD_C = 100;
+    public static final double Y_NEIGHBOURHOOD_C = 100; //initialize the location of living area C
+    public static final double X_NEIGHBOURHOOD_D = 150;
+    public static final double Y_NEIGHBOURHOOD_D = 150; //initialize the location of living area D
+
+    public static final double X_NEIGHBOURHOOD_B = 0;
+    public static final double Y_NEIGHBOURHOOD_B = 0;
+    public static final long POPULATION_B = 300;
     public static final long POPULATION_C = 100;
-    public static final double DIATANCE_B = Math.sqrt((Math.pow((X_B - X_C), 2) + Math.pow((Y_B - X_C), 2))); //set some attributes for the basic point B, can be modified
+    public static final double DIATANCE_B = Math.sqrt((Math.pow((X_NEIGHBOURHOOD_B - X_NEIGHBOURHOOD_C), 2) + Math.pow((Y_NEIGHBOURHOOD_B - X_NEIGHBOURHOOD_C), 2))); //set some attributes for the basic point B, can be modified
 
     public static double calculateFitnessValue(Location location, double population) { // Reilly's law of retail gravitation
 
         double x = location.getLoc()[0];
         double y = location.getLoc()[1];
-        double distance1 = Math.sqrt((Math.pow((x - X_C), 2) + Math.pow((y - Y_C), 2))); // calculate the distance from C
+        double distance1 = Math.sqrt((Math.pow((x - X_NEIGHBOURHOOD_C), 2) + Math.pow((y - Y_NEIGHBOURHOOD_C), 2))); // calculate the distance from C
         double result = 3000 - Math.pow((distance1 - 30), 2);
         return result;
     }
@@ -53,7 +68,7 @@ public class SwarmFunctions {
         double population = 0;
         double x = location.getLoc()[0];
         double y = location.getLoc()[1];
-        double distance = Math.sqrt((Math.pow((x - X_C), 2) + Math.pow((y - Y_C), 2)));
+        double distance = Math.sqrt((Math.pow((x - X_NEIGHBOURHOOD_C), 2) + Math.pow((y - Y_NEIGHBOURHOOD_C), 2)));
         population = POPULATION_C - distance / 2 ; // cause the distribution of population is linear, so I designed a linear f(x) to calculate population
         if (population > 0) {
             return population;
@@ -65,7 +80,7 @@ public class SwarmFunctions {
     public static double calculateDistance(Location location) {
         double x = location.getLoc()[0];
         double y = location.getLoc()[1];
-        double distance = Math.sqrt((Math.pow((x - X_C), 2) + Math.pow((y - Y_C), 2)));
+        double distance = Math.sqrt((Math.pow((x - X_NEIGHBOURHOOD_C), 2) + Math.pow((y - Y_NEIGHBOURHOOD_C), 2)));
         return distance;
     }
     
@@ -75,6 +90,32 @@ public class SwarmFunctions {
         Random generator = new Random();
         for (int i = 0; i < SWARM_SIZE; i++) {
             Particle p = new Particle();
+                        // randomize location inside a space defined in Problem Set
+            double[] loc = new double[PROBLEM_DIMENSION];
+            loc[0] = SwarmFunctions.S_X_LOW_LOC + generator.nextDouble() * (SwarmFunctions.S_X_HIGH_LOC - SwarmFunctions.S_X_LOW_LOC);
+            loc[1] = SwarmFunctions.S_Y_LOW_LOC + generator.nextDouble() * (SwarmFunctions.S_Y_HIGH_LOC - SwarmFunctions.S_Y_LOW_LOC);
+            Location location = new Location(loc);
+
+            // randomize velocity in the range defined in Problem Set
+            double[] vel = new double[PROBLEM_DIMENSION];
+            vel[0] = SwarmFunctions.PAR_LOW_VEL + generator.nextDouble() * (SwarmFunctions.PAR_HIGH_VEL - SwarmFunctions.PAR_LOW_VEL);
+            vel[1] = SwarmFunctions.PAR_LOW_VEL + generator.nextDouble() * (SwarmFunctions.PAR_HIGH_VEL - SwarmFunctions.PAR_LOW_VEL);
+            Velocity velocity = new Velocity(vel);
+
+            //at the very beginning, the pBest is current particle itself
+            PBest pBest = new PBest();
+            pBest.setId(i);
+            pBest.setLocation(location);
+            pBest.setFitnessValue(calculateFitnessValue(location, calculatePopulation(location)));
+
+            p.setLocation(location);
+            p.setVelocity(velocity);
+            p.setId(i);
+            p.setPopulation(calculatePopulation(location));
+            p.setDistance(calculateDistance(location));
+            p.setpBest(pBest);
+
+            swarm.add(p);
 
         }
         return swarm;
